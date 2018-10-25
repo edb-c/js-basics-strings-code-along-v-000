b91dede4692c729cd08172dc8192108053af4bc7
Ethel C <cofresi.ethel@gmail.com>
2018-10-25 04:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Write your code in this file!
 
 const currentUser = 'Grace Hopper';
-const welcomeMessage = 'Welcome to Flatbook, ';
+
+
+
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
